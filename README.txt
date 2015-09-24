087b84dfdb791f6d27cf93d78563489c4d72d168
dkmsntua <dkms.ntua@gmail.com>
2015-09-24 14:09:25 +0300

    license changed to apache 2

--- README.txt
+++ README.txt
@@ -1,18 +1,4 @@
-	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-==========================================================================
-
+	
 You can find all the necessary files to run the project in the "codeFiles" folder.
 You can find directions on how to get started at "documentation -> htmls -> guide.html".
 
--- documentation/scripts/easyTooltip.js
+++ documentation/scripts/easyTooltip.js
@@ -1,16 +1,3 @@
-/*
- * 	Easy Tooltip 1.0 - jQuery plugin
- *	written by Alen Grakalic	
- *	http://cssglobe.com/post/4380/easy-tooltip--jquery-plugin
- *
- *	Copyright (c) 2009 Alen Grakalic (http://cssglobe.com)
- *	Dual licensed under the MIT (MIT-LICENSE.txt)
- *	and GPL (GPL-LICENSE.txt) licenses.
- *
- *	Built for jQuery library
- *	http://jquery.com
- *
- */
 (function($){
 	$.fn.easyTooltip = function(options){
 		var defaults = {
@@ -62,8 +49,8 @@
 						if (content != "" && content != undefined)
 						{
 							$("body").append("<div id='" + options.tooltipId + "'>" + content + "</div>");
-							$("#" + options.tooltipId).css("position", "absolute").css("top", (e.pageY - options.yOffset) + "px").css("left",
-									(e.pageX + options.xOffset) + "px").css("display", "none").fadeIn("fast")
+							$("#" + options.tooltipId).css("position", "absolute").css("top", (e.pageY - options.yOffset) + "px").css("left", (e.pageX + options.xOffset) + "px")
+									.css("display", "none").fadeIn("fast")
 						}
 					}, function(){
 						$("#" + options.tooltipId).remove();
